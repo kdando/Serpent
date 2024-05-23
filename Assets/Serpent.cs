baca16f6bc00d5c3389173a92301d79e698c12a9
kdando <kvdando@gmail.com>
2024-05-23 18:28:47 +0100

    Snake movement script

---
+++ Assets/Serpent.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Serpent : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+    }
+
+}
